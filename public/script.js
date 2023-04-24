0d1627eea2662d83efe47c4a5a3fdfc2d645f6fe
mahtabesmaeili1 <mahtabesmaeilii13@gmail.com>
2023-04-24 12:59:05 +0200

    shows my own video on the screen now

--- public/script.js
+++ public/script.js
@@ -1,10 +1,31 @@
 const socket = io("/");
-const mypeer = new Peer(undefined, {
+const videoGrid = document.getElementById("video-grid");
+
+const myPeer = new Peer(undefined, {
   host: "/",
   port: "3001",
 });
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    addvideoStream(myVideo, stream);
+  });
+myPeer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
 
-socket.emit("join-room", ROOM_ID, 10);
 socket.on("user-connected", (userId) => {
   console.log("User connected: " + userId);
 });
+function addvideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+}
