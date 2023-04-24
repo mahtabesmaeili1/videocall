934e41a8e27d6537f1bc77c29bf806038e275d61
mahtabesmaeili1 <mahtabesmaeilii13@gmail.com>
2023-04-24 12:44:55 +0200

    we are connected to the peer server now

--- public/script.js
+++ public/script.js
@@ -1,4 +1,9 @@
 const socket = io("/");
+const mypeer = new Peer(undefined, {
+  host: "/",
+  port: "3001",
+});
+
 socket.emit("join-room", ROOM_ID, 10);
 socket.on("user-connected", (userId) => {
   console.log("User connected: " + userId);
